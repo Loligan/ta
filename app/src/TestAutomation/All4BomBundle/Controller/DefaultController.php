01f91838ed1eecfac8329bd7612b102dd82c474c
loligan <tfo2011@gmail.com>
2017-07-20 12:46:21 +0300

    fix

--- app/src/TestAutomation/All4BomBundle/Controller/DefaultController.php
+++ app/src/TestAutomation/All4BomBundle/Controller/DefaultController.php
@@ -8,6 +8,19 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('TestAutomationAll4BomBundle:Default:index.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $testResultsRep = $em->getRepository('TestAutomationAll4BomBundle:TestResult');
+//
+        $countTests = $testResultsRep->createQueryBuilder('u')
+            ->select('count(u.id)')->getQuery()->getSingleResult();
+        $countFail = $testResultsRep->createQueryBuilder('u')
+            ->select('count(u.id)')
+            ->where('u.failStep IS NOT NULL')->getQuery()->getSingleResult();
+        return $this->render('TestAutomationAll4BomBundle:Default:index.html.twig',
+            [
+                "count_tests"=>$countTests[1],
+                "count_fail_tests"=>$countFail[1],
+            ]
+        );
     }
 }
